169329d4ef9866f88b9916d46db24903ff1115de
sunhao <623784007@qq.com>
2018-07-29 18:38:10 +0800

    20180729 hom

---
+++ src/main/java/com/shuchen/HelloTest.java
@@ -0,0 +1,13 @@
+package com.shuchen;
+
+import org.junit.Test;
+
+public class HelloTest {
+	public static void main(String[] args) {
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("do");
+	}
+}
